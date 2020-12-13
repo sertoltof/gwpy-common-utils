e2e44736cd067d20136301268c6bc119e6609ef5
Sertoltof <white.garrick935@gmail.com>
2020-12-12 20:07:00 -0500

    I had forgotten setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='gwpy-common-utils',
+    version='0.1',
+    packages=['common_utils'],
+    url='https://github.com/sertoltof/gwpy-common-utils',
+    license='MIT',
+    author='Garrick White',
+    author_email='white.garrick935@gmail.com',
+    description='common python code',
+    requires=[]
+)
